bba2dfba29e965b0b92bfc6205c27ca324be7ecb
KimTaewan <wwan13@naver.com>
2020-11-28 18:43:37 +0900

    음료의 정보를 담은 클래스 BeverageInfo

---
+++ vmServer/src/server/BeverageInfo.java
@@ -0,0 +1,19 @@
+package server;
+
+// 음료들의 정보르 담고 있는 클래스
+public class BeverageInfo {
+    String water = "water";
+    Integer water_price = 450;
+
+    String coffee = "coffee";
+    Integer coffee_price = 500;
+
+    String sports_drink = "sports drink";
+    Integer sports_drink_price = 550;
+
+    String premium_coffee = "premium coffee";
+    Integer premium_coffee_price = 700;
+
+    String soda = "soda";
+    Integer soda_price = 750;
+}
